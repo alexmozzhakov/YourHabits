fc2e03e360dbccdb104fea40bf380e950fb30263
alexmozzhakov <alexey.m@ex.ua>
2016-07-05 06:30:36 +0300

    v0.5.1

--- app/src/main/java/com/doHabit/activity/PasswordRecoveryActivity.java
+++ app/src/main/java/com/doHabit/activity/PasswordRecoveryActivity.java
@@ -1,15 +1,17 @@
 package com.dohabit.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
+import com.dohabit.R;
 import com.dohabit.fragments.RegisterFragment;
 import com.google.firebase.auth.FirebaseAuth;
-import com.dohabit.R;
 
 public class PasswordRecoveryActivity extends AppCompatActivity {
 
@@ -36,6 +38,13 @@ public class PasswordRecoveryActivity extends AppCompatActivity {
             finish();
         } else {
             Toast.makeText(getApplicationContext(), "Recovery email isn't valid", Toast.LENGTH_SHORT).show();
+        }
+
+        final InputMethodManager imm = (InputMethodManager) getApplicationContext()
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        if (getCurrentFocus() != null) {
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
         }
     }
 }
