0870c421fce547fb82b02fcc811ea35f2ea5abff
alexmozzhakov <alexey.m@ex.ua>
2017-12-04 21:17:47 +0200

    Adding test for NumberFormatException.

---
+++ app/src/androidTest/java/com/doapps/habits/activity/IntegerOverflowTest.java
@@ -0,0 +1,42 @@
+package com.doapps.habits.activity;
+
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.swipeRight;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import com.doapps.habits.R;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import tools.fastlane.screengrab.locale.LocaleTestRule;
+
+@RunWith(AndroidJUnit4.class)
+public class IntegerOverflowTest {
+
+  @ClassRule
+  public static final LocaleTestRule localeTestRule = new LocaleTestRule();
+
+  @Rule
+  public ActivityTestRule<MainActivity> mainActivityActivityTestRule =
+      new ActivityTestRule<>(MainActivity.class);
+
+  @Test
+  public void createAndDeleteHabit() {
+    onView(withId(R.id.fab)).perform(click());
+    onView(withId(R.id.edit_title)).perform(typeText("Running"));
+    onView(withId(R.id.edit_time)).perform(typeText("50234902384329048324932423"));
+    onView(withId(R.id.edit_time)).perform(pressImeActionButton());
+    closeSoftKeyboard();
+    onView(withId(R.id.btnCreate)).perform(click());
+    onView(withId(R.id.habits_list))
+        .perform(RecyclerViewActions.actionOnItemAtPosition(0, swipeRight()));
+  }
+}
